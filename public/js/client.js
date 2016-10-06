b5d835a643e1d62cc645873fae0a871bb4496fc9
Vaughn Prosser <vaughnprosser@gmail.com>
2016-10-06 08:10:05 -0500

    completed challenge

--- public/js/client.js
+++ public/js/client.js
@@ -1,34 +1,57 @@
-$(function(){
-  // ask the server for songs, and then draw them
-  getSongs();
-
-  // listen for submit events and send new songs to the server
-  $('form').on('submit', function(event){
-    event.preventDefault();
-    var formData = $(this).serialize();
-    $.ajax({
-      type: 'POST',
-      url: '/songs',
-      data: formData,
-      success: getSongs
-    });
-
-    $(this).find('input[type=text]').val('');
-  });
+$(function() {
+	// ask the server for songs, and then draw them
+	getSongs();
+	// listen for submit events and send new songs to the server
+	$('form').on('submit', function(event) {
+		event.preventDefault();
+		var formData = $(this).serialize();
+		$.ajax({
+			type: 'POST',
+			url: '/songs',
+			data: formData,
+			success: getSongs,
+			statusCode: {
+				403: function() {
+					alert('You have blank fields!')
+				},
+				409: function() {
+					alert('That song is already here!')
+				}
+			}
+		});
+		$(this).find('input[type=text]').val('');
+	});
+	$('ul').on('click', 'button', function() {
+		console.log('I was clicked');
+		var index = $(this).attr('id');
+		$.ajax({
+			type: 'POST',
+			url: '/songs/delete',
+			data: 'index=' + index,
+			success: function() {
+				$('#songs').empty();
+				getSongs();
+			}
+		})
+	})
 });
 
 function getSongs() {
-  $.ajax({
-    type: 'GET',
-    url: '/songs',
-    success: function(songs){
-      $('#songs').empty();
-      songs.forEach(function(song){
-        var $li = $('<li></li>');
-        $li.append('<p>'+ song.title + '</p>');
-        $li.append('<p>by: '+ song.artist + '</p>');
-        $('#songs').append($li);
-      });
-    }
-  });
+	$.ajax({
+		type: 'GET',
+		url: '/songs',
+		success: function(songs) {
+			$('#songs').empty();
+			var i = 0;
+			songs.forEach(function(song) {
+				var $li = $('<li></li>');
+				$li.append('<p>' + song.title + '</p>');
+				$li.append('<p>by: ' + song.artist + '</p>');
+				$li.append('<p>Time added: ' + song.date + '</p>')
+				$li.append('<button class="delete" id="' + i + '">Delete</button></li>')
+				$('#songs').append($li);
+				i++;
+			});
+		}
+	});
 }
--- server.js
+++ server.js
@@ -1,54 +1,70 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-
 var app = express();
-
 // middleware function, gets executed on each request
-app.use(function(req, res, next){
-  console.log('Got a request!');
-  next();
+app.use(function(req, res, next) {
+	console.log('Got a request!');
+	next();
 });
-
 // middleware for parsing the body and turning it into a JS object
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.post('/', function(req, res){
-  console.log('req.body=', req.body);
-  res.sendStatus(200);
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.post('/', function(req, res) {
+	console.log('req.body=', req.body);
+	res.sendStatus(200);
 });
-
-app.get('/', function(req, res){
-  console.log('Received a request at', new Date());
-  // __dirname is the folder this file lives in
-  var filename = path.join(__dirname, 'public/views/index.html');
-  console.log('filename:', filename);
-  res.sendFile(filename);
+app.get('/', function(req, res) {
+	console.log('Received a request at', new Date());
+	// __dirname is the folder this file lives in
+	var filename = path.join(__dirname, 'public/views/index.html');
+	console.log('filename:', filename);
+	res.sendFile(filename);
 });
-
-app.get('/kittens', function(req, res){
-  console.log('Query params:', req.query);
-  if (req.query.age > 2) {
-    res.send('MEOW!');
-  } else {
-    res.send('meow');
-  }
-});
-
 var songs = [];
-
-app.post('/songs', function(req, res){
-  console.log('req.body:', req.body);
-  songs.push(req.body);
-  console.log('songs', songs);
-  res.sendStatus(200);
+app.post('/songs', function(req, res) {
+	var d = new Date();
+	var repeat;
+	songs.forEach(function(index) {
+		if (index.title === req.body.title && index.artist === req.body.artist) {
+			repeat = true;
+		} else {
+			repeat = false;
+		}
+	});
+	console.log(repeat);
+	console.log('req.body:', req.body);
+	if (req.body.title == '' || req.body.artist == '') {
+		res.sendStatus(403);
+	} else if (repeat) {
+		res.sendStatus(409);
+	} else {
+		req.body.date = d.toString();
+		songs.push(req.body);
+		console.log('songs', songs);
+		res.sendStatus(200);
+	}
 });
-
-app.get('/songs', function(req, res){
-  res.send(songs);
+app.post('/songs/delete', function(req, res) {
+	var body = req.body;
+	console.log(body);
+	songs.splice(body.index, body.index + 1);
+	console.log(songs);
+	res.sendStatus(200);
+})
+app.get('/songs', function(req, res) {
+	res.send(songs);
 });
-
 // middleware for serving static files
 app.use(express.static('public'));
-
 app.listen(3000);
+//
+// app.get('/kittens', function(req, res){
+//   console.log('Query params:', req.query);
+//   if (req.query.age > 2) {
+//     res.send('MEOW!');
+//   } else {
+//     res.send('meow');
+//   }
+// });
